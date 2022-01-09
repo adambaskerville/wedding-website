c3d5bf711dc85b36e4455fbe640e5198e37d093d
Adam <ab695@sussex.ac.uk>
2022-01-09 16:36:42 +0000

    Added latest changes.
    Fixed calendar dates and moved the schedule ot a link rather than image.

--- js/scripts.js
+++ js/scripts.js
@@ -186,14 +186,14 @@ $(document).ready(function () {
             title: "Charlotte and Adam's Wedding",
 
             // Event start date
-            start: new Date('July 05, 2022 10:00'),
+            start: new Date('July 05, 2022 13:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('July 06, 2022 00:00'),
 
             // Event Address
             address: 'Rivervale Barn, Yateley',
